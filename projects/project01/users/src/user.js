d854c2e20a607f7a5a6602b95d9e882cdf6b49f5
Tom Scott <tomscott85@gmail.com>
2017-01-24 20:12:16 +1100

    update sections 6,7 & 8

--- projects/project01/users/src/user.js
+++ projects/project01/users/src/user.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
+const PostSchema = require('./post');
 const Schema = mongoose.Schema;
 
 // in future should create schema's in the schema folder...
 const UserSchema = new Schema({
-  name: String
+  name: {
+    type: String,
+    validate: {
+      validator: (name) => name.length > 2,
+      message: 'Name must be longer than 2 characters.'
+    },
+    required: [true, 'Name is required.']
+  },
+  postCount: Number,
+  posts: [PostSchema]
 });
 
 const User = mongoose.model('user', UserSchema);
